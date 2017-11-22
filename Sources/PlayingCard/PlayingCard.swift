5cda2f3c97e11d63b7e28d5428314354ed10cda4
Ryan Sobol <contact@ryansobol.com>
2017-11-22 06:06:08 -0500

    Fix compile error

--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -2,7 +2,7 @@ public struct PlayingCard {
   let rank: Rank
   let suit: Suit
 
-  public init(rank: rank, suit: suit) {
+  public init(rank: Rank, suit: Suit) {
     self.rank = rank
     self.suit = suit
   }
